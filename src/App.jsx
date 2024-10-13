e15c2a5e394dffaaafb2852e695f365fcaacec5e
WedSal <wed.salman18@gmail.com>
2024-10-13 14:58:54 +0200

    DogListFixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
-import CatalogPage from './CatalogPage'; 
+import CatalogPage from './CatalogPage';
 import DogDetails from './DogDetails';
 
 
 
 function App() {
-  
+
 
   return (
     <Router>
       <div className='app-container'>
-      <header className="header">
-        <img src="" alt="" className="logo"/>
-        <nav className="nav">
-          <Link to="/"className='nav-link'>Home</Link>
-         <Link to="/catalog"className='nav-link'>Catalog</Link>
-          <Link to="/contact"className='nav-link'>Contact Us</Link>
-          <Link to="/about"className='nav-link'>About Us</Link>
-         <Link to="/social"className='nav-link'>Social Media</Link>
-        </nav>
-       </header>
+        <header className="header">
+          <img src="" alt="" className="logo" />
+          <nav className="nav">
+            <Link to="/" className='nav-link'>Home</Link>
+            <Link to="/catalog" className='nav-link'>Catalog</Link>
+            <Link to="/contact" className='nav-link'>Contact Us</Link>
+            <Link to="/about" className='nav-link'>About Us</Link>
+            <Link to="/social" className='nav-link'>Social Media</Link>
+          </nav>
+        </header>
 
 
-         <main className='main-content'>
-        <Routes>
-         <Route path="/" element={<WelcomePage />} />  
-         <Route path="/catalog" element={<CatalogPage />} />
-         <Route path="/dog/:id" element={<DogDetails/>} />
-        </Routes>
+        <main className='main-content'>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/dog/:id" element={<DogDetails />} />
+          </Routes>
         </main>
       </div>
-         </Router>
+    </Router>
   );
 }
 
--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -1,16 +1,16 @@
-import React from "react"; 
+import React from "react";
 import { Link } from "react-router-dom";
-import "./App.css"; 
+import "./App.css";
 
 
 function WelcomePage() {
     return (
         <div className="hero">
             <div className="hero-content">
-            <h1 className="title">Welcome to Doggy Daycare!</h1>
-            <p className="subtitle">Your Dog's second Home!</p>
-            <Link to="/catalog" className="button">See All Dogs</Link>
-             </div>
+                <h1 className="title">Welcome to Doggy Daycare!</h1>
+                <p className="subtitle">Your Dog's second Home!</p>
+                <Link to="/catalog" className="button">See All Dogs</Link>
+            </div>
 
         </div>
     );
